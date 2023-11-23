461628f96a44a059e828b5f1bf62413017999b1c
kenGwon <kgh9959@gmail.com>
2023-11-23 17:56:57 +0900

    클라이언트 TCP/IP 송신 패킷 구조 3개 송신 -> 2개 송신

--- EOF_NUGUSEM_CLIENT/raspberrypi/raspberrypi.py
+++ EOF_NUGUSEM_CLIENT/raspberrypi/raspberrypi.py
@@ -1,61 +1,129 @@
-#RFID가 리더기에 읽히면 아두이노 측에서 라즈베리파이로 UID 송신하고 수신받은 #라즈베리파이는 윈도우 서버로 송신하는 코드
-import serial
+
 import socket
 import struct
-
+import threading
+import serial
+import time
 class TcpClient:
     def __init__(self, server_ip, port):
         self.server_ip = server_ip
         self.port = port
+        self.client_socket = None
 
     def connect_to_server(self):
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_socket.connect((self.server_ip, self.port))
-        print("Connected to the server")
-        return client_socket
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.client_socket.connect((self.server_ip, self.port))
+            print("서버에 연결되었습니다.")
+        except ConnectionRefusedError:
+            print("서버에 연결할 수 없습니다.")
+            self.client_socket = None  # 연결 실패 시 client_socket을 None으로 설정
+    def wait_for_ACK(self):
+        ACK = 9
+        ack_type_header = self.client_socket.recv(4)
 
-    def send_data_type(self, client_socket, data_type):
-        data_type_header = struct.pack("I", data_type)
-        client_socket.sendall(data_type_header)
+        # Check if received data is empty
+        if not ack_type_header:
+            print("Error: No data received.")
+            return
 
-    def send_string(self, message):
-        client_socket = self.connect_to_server()
-        self.send_data_type(client_socket, 1)  # STRING
-        client_socket.sendall(message.encode("utf-8"))
-        print("String sent to server")
-        client_socket.close()
+        ack_type = struct.unpack("I", ack_type_header)[0]
+        if ack_type == ACK:
+            print("ACK received")
+        else:
+            print("ACK reception failed")
+
+
+    def send_data_type(self, data_type):
+        if self.client_socket is not None:  # client_socket이 None이 아닌지 확인
+            try:
+                data_type_header = struct.pack("I", data_type)
+                self.client_socket.sendall(data_type_header)
+            except AttributeError:
+                print("데이터 유형을 보낼 수 없습니다.")
+        else:
+            print("클라이언트 소켓이 초기화되지 않았습니다. 연결이 실패했을 수 있습니다.")
 
     def send_image(self, image_path):
-        client_socket = self.connect_to_server()
-        self.send_data_type(client_socket, 0)  # IMAGE
+        self.connect_to_server()
+        self.send_data_type(0)  # 이미지
+
+        # 이미지 전송
         with open(image_path, "rb") as image_file:
-            image_data = image_file.read()
-            client_socket.sendall(image_data)
-        print("Image sent to server")
-        client_socket.close()
+            while True:
+                image_data = image_file.read(1024)
+                if not image_data:
+                    last_packet=struct.pack("I",9)
+                    self.client_socket.sendall(last_packet)
+                    break
+                self.client_socket.sendall(image_data)
+        print("이미지를 서버로 보냈습니다.")
 
-    def send_rfid_uid(self, uid):
-        client_socket = self.connect_to_server()
-        self.send_data_type(client_socket, 2)  # RFID_UID
-        client_socket.sendall(uid.encode("utf-8"))
-        print("RFID UID sent to server")
-        client_socket.close()
+    def receive_data_type(self):
+        # 데이터 유형 수신
+        data_type_header = self.client_socket.recv(4)
 
+        # 데이터 언팩에 충분한 데이터가 있는지 확인
+        if len(data_type_header) < 4:
+            print("오류: 데이터 언팩에 충분한 데이터가 없습니다.")
+            return -1
 
+        data_type = struct.unpack("I", data_type_header)[0]
+        return data_type
 
-if __name__ == "__main__":
-    tcp_client = TcpClient("10.10.15.58", 8888)
+    def close_connection(self):
+        self.client_socket.close()
 
+def receive_uid_and_send_image():
     # Arduino에서 UID 수신 및 서버로 전송
     ser = serial.Serial('/dev/ttyACM0', 9600)
-    while True:
-        data = ser.readline().decode('utf-8').strip()
-        if data and data.startswith("U"):
-            uid = data[1:]  # 헤더 "U"를 제외한 부분이 UID
-            print("Received UID:", uid)
 
-            # 여기에 UID를 사용하여 원하는 동작 수행
-            # 예: 데이터베이스 조회, 특정 동작 수행 등
+    try:
+        while True:
+            data = ser.readline().decode('utf-8').strip()
+            if data and data.startswith("U"):
+                uid = data[1:]  # 헤더 "U"를 제외한 부분이 UID
+                print("받은 UID:", uid)
 
-            # RFID UID를 서버로 전송
-            tcp_client.send_rfid_uid(uid)
+                # RFID UID를 서버로 전송
+                tcp_client = TcpClient("10.10.15.58", 8888)
+                try:
+                    tcp_client.connect_to_server()
+                    
+                    
+                    #tcp_client.send_data_type(2)  # RFID_UID
+                    #tcp_client.client_socket.sendall(uid.encode("utf-8"))
+                    #print("RFID UID를 서버로 보냈습니다.")
+                    #tcp_client.wait_for_ACK()
+                    
+                    
+                    tcp_client.send_data_type(1)  # Log string
+                    Log = uid + "@" + "Log Test"
+                    
+                    tcp_client.client_socket.sendall(Log.encode("utf-8"))
+                    print("Log를 서버로 보냈습니다.")
+                    tcp_client.wait_for_ACK()
+                    
+                    
+                    
+                    tcp_client.send_data_type(0)  # image
+                    tcp_client.send_image("gcc_version.png")# 이미지를 서버로 송신
+                    tcp_client.wait_for_ACK()
+                    time.sleep(1)
+                    
+
+                    # 대기: 이미지 전송이 시작됨을 확인
+                    #tcp_client.receive_data_type()
+
+                except Exception as e:
+                    print(f"통신 오류: {e}")
+                finally:
+                    tcp_client.close_connection()
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt: 데이터 수신을 중지합니다.")
+    finally:
+        ser.close()
+
+if __name__ == "__main__":
+    receive_uid_and_send_image()
+
